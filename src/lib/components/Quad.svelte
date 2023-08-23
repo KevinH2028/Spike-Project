9ac2fa501fbb2cd60ba03c7ff7fa544fb2fdce23
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-08-23 13:02:53 -0400

    Start lessons + Components

---
+++ src/lib/components/Quad.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let data;
+</script>
+
+<div class="h-screen flex flex-col">
+  <!-- This is for the H1 to be centered in the top half of the viewport -->
+  <div class="flex-1 flex items-center justify-center">
+    <h1>{data.text}</h1>
+  </div>
+
+  <!-- This is for the buttons on the bottom half of the viewport -->
+  <div class="flex-1 flex justify-around items-center flex-wrap">
+    <!-- Button styles have been modified to meet your requirements -->
+    <button
+      class="flex-1 m-5 px-4 py-2 bg-blue-500 text-white rounded max-h-[50vh]"
+    >
+      {data.options[0]}
+    </button>
+
+    <button
+      class="flex-1 m-5 px-4 py-2 bg-green-500 text-white rounded max-h-[50vh]"
+    >
+      {data.options[1]}
+    </button>
+
+    <button
+      class="flex-1 m-5 px-4 py-2 bg-yellow-500 text-white rounded max-h-[50vh]"
+    >
+      {data.options[2]}
+    </button>
+
+    <button
+      class="flex-1 m-5 px-4 py-2 bg-red-500 text-white rounded max-h-[50vh]"
+    >
+      {data.options[3]}
+    </button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+  import { activities, currentActivityIndex } from "$lib/stores.js";
+
+  import Lesson from "$lib/components/Lesson.svelte";
+  import Pills from "$lib/components/Pills.svelte";
+  import Quad from "$lib/components/Quad.svelte";
+
+  async function fetchData() {
+    try {
+      const response = await fetch("/activities.json");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      activities.set(await response.json());
+    } catch (error) {
+      console.error(
+        "There was a problem with the fetch operation:",
+        error.message
+      );
+    }
+  }
+
+  onMount(fetchData);
+</script>
+
+{#if $activities.length > 0}
+  <pre>{JSON.stringify($activities, null, 2)}</pre>
+
+  {#if $activities[$currentActivityIndex].type === "lesson"}
+    <Lesson />
+  {:else if $activities[$currentActivityIndex].type === "pills"}
+    <Pills />
+  {:else if $activities[$currentActivityIndex].type === "quad"}
+    <p>G</p>
+    <Quad data={$activities[$currentActivityIndex].content} />
+  {/if}
+{:else}
+  <p>Loading...</p>
+{/if}
